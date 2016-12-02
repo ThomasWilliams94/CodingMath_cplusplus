418f7d63ca1c8c55763711123decf74bc9c8e918
Thomas Williams <thomas.williams@live.co.uk>
2016-12-02 18:14:08 +0100

    Square now moves in a lissajous curve.

--- Episode4/src/main.cpp
+++ Episode4/src/main.cpp
@@ -30,24 +30,27 @@ int main(int argc, char* argv[]) {
 	double centerX = screen.SCREEN_WIDTH * 0.5;
 	double xRadius = 400;
 	double yRadius = 200;
-	double speed = 0.07;
-	double angle = 0.0;
+	double xSpeed = 0.1;
+	double ySpeed = 0.131;
+	double xAngle = 0.0;
+	double yAngle = 0.0;
 	double x, y = 0.0;
-	double width = 25.0;
+	double width = 10.0;
 
 	// While loop used to keep track of if user has clicked x to quit
 	while (true) {
 
-		screen.clear();
+		//screen.clear();
 
-		x = centerX + cos(angle) * xRadius;
-		y = centerY + sin(angle) * yRadius;
+		x = centerX + cos(xAngle) * xRadius;
+		y = centerY + sin(yAngle) * yRadius;
 
 		screen.drawRect(x - width/2, y - width/2, width, width);
 
 		screen.present();
 
-		angle += speed;
+		xAngle += xSpeed;
+		yAngle += ySpeed;
 
 		// Check for messages/events
 		if (!screen.processEvents()) {
