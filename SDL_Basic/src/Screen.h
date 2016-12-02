6d12d981d59f3417b464a09c018e6e8f062f3f65
Thomas Williams <thomas.williams@live.co.uk>
2016-12-02 15:50:10 +0100

    Adding basic SDL project, from which later projects can be developed.

---
+++ SDL_Basic/src/Screen.h
@@ -0,0 +1,40 @@
+/*
+ * Screen.h
+ *
+ *  Created on: 6 Nov 2016
+ *      Author: Thomas
+ */
+
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+#include <SDL.h>
+
+namespace codingmath {
+
+class Screen {
+public:
+	const static int SCREEN_WIDTH = 800;
+	const static int SCREEN_HEIGHT = 600;
+
+private:
+	SDL_Window *m_window;
+	SDL_Renderer *m_renderer;
+	SDL_Texture *m_texture;
+	Uint32 *m_buffer1;
+	Uint32 *m_buffer2;
+
+public:
+	Screen();
+	bool init();
+	void update();
+	void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
+	bool processEvents();
+	void close();
+	void clear();
+	void boxBlur();
+};
+
+} /* namespace codingmath */
+
+#endif /* SCREEN_H_ */
